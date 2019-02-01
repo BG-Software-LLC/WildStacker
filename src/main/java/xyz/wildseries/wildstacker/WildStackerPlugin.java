f1526659ce9ea4e706508fe81ab4df9b73e585d3
OmerBenGera <omerthepro126@gmail.com>
2019-02-01 13:54:22 +0200

    Added mcMMO listener to multiply xp

--- src/main/java/xyz/wildseries/wildstacker/WildStackerPlugin.java
+++ src/main/java/xyz/wildseries/wildstacker/WildStackerPlugin.java
@@ -108,7 +108,7 @@ public final class WildStackerPlugin extends JavaPlugin implements WildStacker {
             if(getServer().getPluginManager().isPluginEnabled("SilkSpawners"))
                 getServer().getPluginManager().registerEvents(new SilkSpawnersListener(this), this);
             if(getServer().getPluginManager().isPluginEnabled("EpicSpawners"))
-                getServer().getPluginManager().registerEvents(new EpicSpawnersListener(this), this);;
+                getServer().getPluginManager().registerEvents(new EpicSpawnersListener(this), this);
             if(getServer().getPluginManager().isPluginEnabled("mcMMO"))
                 getServer().getPluginManager().registerEvents(new McMMOListener(), this);
 
