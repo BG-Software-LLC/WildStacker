3e869db56cf06b48939bc719ee8c930229f163eb
OmerBenGera <omerthepro126@gmail.com>
2019-02-26 23:28:14 +0200

    Added MythicMobsHook to have a getDrops method

--- src/main/java/com/bgsoftware/wildstacker/hooks/MythicMobsHook.java
+++ src/main/java/com/bgsoftware/wildstacker/hooks/MythicMobsHook.java
@@ -4,6 +4,10 @@ import io.lumine.xikage.mythicmobs.MythicMobs;
 import io.lumine.xikage.mythicmobs.mobs.ActiveMob;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.LivingEntity;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public final class MythicMobsHook {
 
@@ -18,11 +22,27 @@ public final class MythicMobsHook {
     public static LivingEntity tryDuplicate(LivingEntity livingEntity){
         if(isMythicMob(livingEntity)){
             ActiveMob activeMob = MythicMobs.inst().getMobManager().getMythicMobInstance(livingEntity);
-            ActiveMob duplicate = MythicMobs.inst().getMobManager().spawnMob(activeMob.getEntity().getName(), livingEntity.getLocation());
+            ActiveMob duplicate = MythicMobs.inst().getMobManager().spawnMob(activeMob.getType().getInternalName(), livingEntity.getLocation());
             return duplicate.getLivingEntity();
         }
 
         return null;
+    }
+
+    public static List<ItemStack> getDrops(LivingEntity livingEntity, int stackAmount){
+        List<ItemStack> drops = new ArrayList<>();
+
+//        if (isMythicMob(livingEntity)) {
+//            for(int i = 0; i < stackAmount; i++) {
+//                ActiveMob activeMob = MythicMobs.inst().getMobManager().getMythicMobInstance(livingEntity);
+//                Optional<DropTable> dropTable = MythicMobs.inst().getDropManager().getDropTable(activeMob.getType().getInternalName());
+//                dropTable.ifPresent(dropTable1 -> {
+//                    LootBag lootBag = dropTable1.generate(new DropMetadata(activeMob, activeMob.getEntity())).equip(activeMob.getEntity());
+//                });
+//            }
+//        }
+
+        return drops;
     }
 
 }
