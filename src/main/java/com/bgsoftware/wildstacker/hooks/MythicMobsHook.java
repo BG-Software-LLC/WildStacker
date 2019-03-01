6174b4a8b394459857c128f0c6e1849ff794bc9a
OmerBenGera <omerthepro126@gmail.com>
2019-03-01 18:18:27 +0200

    Cleaned code

--- src/main/java/com/bgsoftware/wildstacker/hooks/MythicMobsHook.java
+++ src/main/java/com/bgsoftware/wildstacker/hooks/MythicMobsHook.java
@@ -4,10 +4,6 @@ import io.lumine.xikage.mythicmobs.MythicMobs;
 import io.lumine.xikage.mythicmobs.mobs.ActiveMob;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.LivingEntity;
-import org.bukkit.inventory.ItemStack;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public final class MythicMobsHook {
 
@@ -27,22 +23,6 @@ public final class MythicMobsHook {
         }
 
         return null;
-    }
-
-    public static List<ItemStack> getDrops(LivingEntity livingEntity, int stackAmount){
-        List<ItemStack> drops = new ArrayList<>();
-
-//        if (isMythicMob(livingEntity)) {
-//            for(int i = 0; i < stackAmount; i++) {
-//                ActiveMob activeMob = MythicMobs.inst().getMobManager().getMythicMobInstance(livingEntity);
-//                Optional<DropTable> dropTable = MythicMobs.inst().getDropManager().getDropTable(activeMob.getType().getInternalName());
-//                dropTable.ifPresent(dropTable1 -> {
-//                    LootBag lootBag = dropTable1.generate(new DropMetadata(activeMob, activeMob.getEntity())).equip(activeMob.getEntity());
-//                });
-//            }
-//        }
-
-        return drops;
     }
 
 }
