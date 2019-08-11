61c837c13d4b31130aec40f8b1e92c8af226f159
OmerBenGera <omerthepro126@gmail.com>
2019-08-11 13:14:28 +0300

    Fixed names being updated for blacklisted entities

--- src/main/java/com/bgsoftware/wildstacker/objects/WStackedEntity.java
+++ src/main/java/com/bgsoftware/wildstacker/objects/WStackedEntity.java
@@ -130,6 +130,9 @@ public class WStackedEntity extends WStackedObject<LivingEntity> implements Stac
         if(EntityUtil.isNameBlacklisted(object.getCustomName()))
             return;
 
+        if(isBlacklisted() || !isWhitelisted() || isWorldDisabled())
+            return;
+
         if(getSpawnCause() == SpawnCause.MYTHIC_MOBS && object.getCustomName() != null){
             object.setCustomName(object.getCustomName().replace("{}", String.valueOf(stackAmount)));
             return;
