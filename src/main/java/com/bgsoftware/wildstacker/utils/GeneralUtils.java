32549a08233d45392280ed647df2e2d590f82685
OmerBenGera <omerthepro126@gmail.com>
2019-09-10 16:30:28 +0300

    Added support to 'all' key

--- src/main/java/com/bgsoftware/wildstacker/utils/GeneralUtils.java
+++ src/main/java/com/bgsoftware/wildstacker/utils/GeneralUtils.java
@@ -8,7 +8,8 @@ public final class GeneralUtils {
 
     public static boolean contains(List<String> list, StackedEntity stackedEntity){
         return list.contains(stackedEntity.getType().name()) ||  list.contains(stackedEntity.getSpawnCause().name()) ||
-                list.contains(stackedEntity.getType().name() + ":" + stackedEntity.getSpawnCause().name());
+                list.contains(stackedEntity.getType().name() + ":" + stackedEntity.getSpawnCause().name()) ||
+                list.contains("all") || list.contains("ALL");
     }
 
 }
