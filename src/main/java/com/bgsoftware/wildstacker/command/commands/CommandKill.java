936332154fa5c747be48a0e8c608fc7990a61cf7
OmerBenGera <omerthepro126@gmail.com>
2019-10-26 21:13:35 +0300

    Added an argument for the config filter

--- src/main/java/com/bgsoftware/wildstacker/command/commands/CommandKill.java
+++ src/main/java/com/bgsoftware/wildstacker/command/commands/CommandKill.java
@@ -7,6 +7,7 @@ import com.bgsoftware.wildstacker.command.ICommand;
 import com.bgsoftware.wildstacker.objects.WStackedEntity;
 import com.bgsoftware.wildstacker.utils.Executor;
 import com.bgsoftware.wildstacker.utils.legacy.EntityTypes;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.command.CommandSender;
@@ -30,7 +31,7 @@ public final class CommandKill implements ICommand {
 
     @Override
     public String getUsage() {
-        return "stacker kill [radius=?] [type=?] [spawn=?]";
+        return "stacker kill [radius=?] [type=?] [spawn=?] [config=<true/false>]";
     }
 
     @Override
@@ -50,73 +51,80 @@ public final class CommandKill implements ICommand {
 
     @Override
     public int getMaxArgs() {
-        return 4;
+        return 5;
     }
 
     @Override
     public void perform(WildStackerPlugin plugin, CommandSender sender, String[] args) {
+        if(Bukkit.isPrimaryThread()){
+            Executor.async(() -> perform(plugin, sender, args));
+            return;
+        }
+
         Set<EntityTypes> entityTypes = new HashSet<>();
         Set<SpawnCause> spawnCauses = new HashSet<>();
         IntegerValue integerValue = new IntegerValue(-1);
+        boolean applyTaskFilter = false;
 
-        Executor.async(() -> {
-            if(args.length > 1){
-                for(int i = 1; i < args.length; i++){
-                    if(!args[i].contains("=")){
-                        Locale.COMMAND_USAGE.send(sender, getUsage());
+        if(args.length > 1){
+            for(int i = 1; i < args.length; i++){
+                if(!args[i].contains("=")){
+                    Locale.COMMAND_USAGE.send(sender, getUsage());
+                    return;
+                }
+
+                String value = args[i].split("=")[1];
+                if(args[i].toLowerCase().startsWith("radius=")){
+                    if(!(sender instanceof Player)){
+                        sender.sendMessage(ChatColor.RED + "You must be a player to use the radius filter.");
                         return;
                     }
 
-                    String value = args[i].split("=")[1];
-                    if(args[i].toLowerCase().startsWith("radius=")){
-                        if(!(sender instanceof Player)){
-                            sender.sendMessage(ChatColor.RED + "You must be a player to use the radius filter.");
-                            return;
-                        }
-
-                        try{
-                            integerValue.i = Integer.parseInt(value);
-                        }catch(Exception ignored){
-                            Locale.INVALID_NUMBER.send(sender);
-                            return;
-                        }
+                    try{
+                        integerValue.i = Integer.parseInt(value);
+                    }catch(Exception ignored){
+                        Locale.INVALID_NUMBER.send(sender);
+                        return;
                     }
+                }
 
-                    else if(args[i].toLowerCase().startsWith("type=")){
-                        for(String entityType : value.split(",")){
-                            try {
-                                entityTypes.add(EntityTypes.fromName(entityType.toUpperCase()));
-                            }catch(IllegalArgumentException ex){
-                                Locale.INVALID_ENTITY.send(sender, entityType);
-                                return;
-                            }
-                        }
-                    }
-
-                    else if(args[i].toLowerCase().startsWith("spawn=")){
-                        for(String spawnCause : value.split(",")){
-                            try {
-                                spawnCauses.add(SpawnCause.valueOf(spawnCause));
-                            }catch(IllegalArgumentException ex){
-                                Locale.INVALID_SPAWN_CAUSE.send(sender, spawnCause);
-                                return;
-                            }
+                else if(args[i].toLowerCase().startsWith("type=")){
+                    for(String entityType : value.split(",")){
+                        try {
+                            entityTypes.add(EntityTypes.fromName(entityType.toUpperCase()));
+                        }catch(IllegalArgumentException ex){
+                            Locale.INVALID_ENTITY.send(sender, entityType);
+                            return;
                         }
                     }
                 }
+
+                else if(args[i].toLowerCase().startsWith("spawn=")){
+                    for(String spawnCause : value.split(",")){
+                        try {
+                            spawnCauses.add(SpawnCause.valueOf(spawnCause));
+                        }catch(IllegalArgumentException ex){
+                            Locale.INVALID_SPAWN_CAUSE.send(sender, spawnCause);
+                            return;
+                        }
+                    }
+                }
+
+                else if(args[i].toLowerCase().startsWith("config=")){
+                    applyTaskFilter = Boolean.parseBoolean(value);
+                }
             }
+        }
 
-            Predicate<Entity> entityPredicate = entity ->
-                    (integerValue.i == -1 || inRadius(((Player) sender).getLocation(), entity.getLocation(), integerValue.i)) &&
-                            (entityTypes.isEmpty() || entityTypes.contains(EntityTypes.fromEntity((LivingEntity) entity))) &&
-                            (spawnCauses.isEmpty() || spawnCauses.contains(WStackedEntity.of(entity).getSpawnCause()));
+        Predicate<Entity> entityPredicate = entity ->
+                (integerValue.i == -1 || inRadius(((Player) sender).getLocation(), entity.getLocation(), integerValue.i)) &&
+                        (entityTypes.isEmpty() || entityTypes.contains(EntityTypes.fromEntity((LivingEntity) entity))) &&
+                        (spawnCauses.isEmpty() || spawnCauses.contains(WStackedEntity.of(entity).getSpawnCause()));
 
-            Predicate<Item> itemPredicate = item ->
-                    integerValue.i == -1 || inRadius(((Player) sender).getLocation(), item.getLocation(), integerValue.i);
+        Predicate<Item> itemPredicate = item ->
+                integerValue.i == -1 || inRadius(((Player) sender).getLocation(), item.getLocation(), integerValue.i);
 
-            plugin.getSystemManager().performKillAll(entityPredicate, itemPredicate, true);
-        });
-
+        plugin.getSystemManager().performKillAll(entityPredicate, itemPredicate, applyTaskFilter);
     }
 
     @Override
@@ -135,9 +143,11 @@ public final class CommandKill implements ICommand {
                 list.add("radius=");
             if("spawn=".startsWith(arg.toLowerCase()))
                 list.add("spawn=");
+            if("config=".startsWith(arg.toLowerCase()))
+                list.add("config=");
         }
 
-        if(alreadyFiltered.stream().anyMatch(arg -> !arg.equals("") && !arg.equals("type=") && !arg.equals("radius=") && !arg.equals("spawn="))) {
+        if(alreadyFiltered.stream().anyMatch(arg -> !arg.equals("") && !arg.equals("type=") && !arg.equals("radius=") && !arg.equals("spawn=") && !arg.equals("config="))) {
             list.clear();
         }
 
