5260fdd21d4bcf57e36b58f889c19af0188a0096
OmerBenGera <omerthepro126@gmail.com>
2019-11-28 10:43:53 +0200

    Fixed an issue with Boss plugin

--- src/main/java/com/bgsoftware/wildstacker/listeners/plugins/BossListener.java
+++ src/main/java/com/bgsoftware/wildstacker/listeners/plugins/BossListener.java
@@ -3,6 +3,7 @@ package com.bgsoftware.wildstacker.listeners.plugins;
 import com.bgsoftware.wildstacker.api.enums.SpawnCause;
 import com.bgsoftware.wildstacker.objects.WStackedEntity;
 import com.bgsoftware.wildstacker.utils.entity.EntityUtils;
+import com.bgsoftware.wildstacker.utils.threads.Executor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.mineacademy.boss.api.event.BossSpawnEvent;
@@ -12,7 +13,7 @@ public final class BossListener implements Listener {
     @EventHandler
     public void onBossSpawn(BossSpawnEvent e){
         if(EntityUtils.isStackable(e.getEntity()))
-            WStackedEntity.of(e.getEntity()).setSpawnCause(SpawnCause.BOSS);
+            Executor.sync(() -> WStackedEntity.of(e.getEntity()).setSpawnCause(SpawnCause.BOSS), 2L);
     }
 
 }
