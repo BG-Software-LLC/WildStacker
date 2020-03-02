9890f19c77d9e552ac8dc7acdb97aa996899661e
OmerBenGera <omerthepro126@gmail.com>
2020-03-03 00:23:50 +0200

    Updated stacked spawner to support barrel items

--- API/src/main/java/com/bgsoftware/wildstacker/api/objects/StackedSnapshot.java
+++ API/src/main/java/com/bgsoftware/wildstacker/api/objects/StackedSnapshot.java
@@ -3,6 +3,7 @@ package com.bgsoftware.wildstacker.api.objects;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.entity.EntityType;
+import org.bukkit.inventory.ItemStack;
 
 import java.util.Map;
 
@@ -26,8 +27,18 @@ public interface StackedSnapshot {
      * Get barrel information for a location.
      * @param location The location to get info about.
      * @return Entry that contains the stack size and the type of the barrel.
+     *
+     * @deprecated See getStackedBarrelItem(Location)
      */
+    @Deprecated
     Map.Entry<Integer, Material> getStackedBarrel(Location location);
+
+    /**
+     * Get barrel information for a location.
+     * @param location The location to get info about.
+     * @return Entry that contains the stack size and the item of the barrel.
+     */
+    Map.Entry<Integer, ItemStack> getStackedBarrelItem(Location location);
 
     /**
      * Checks if a location has barrel data.
@@ -47,5 +58,11 @@ public interface StackedSnapshot {
      * @return A map with locations and entries about all barrels.
      */
     Map<Location, Map.Entry<Integer, Material>> getAllBarrels();
+
+    /**
+     * Get all barrels inside the chunk.
+     * @return A map with locations and entries about all barrels.
+     */
+    Map<Location, Map.Entry<Integer, ItemStack>> getAllBarrelsItems();
 
 }
--- src/main/java/com/bgsoftware/wildstacker/objects/WStackedSnapshot.java
+++ src/main/java/com/bgsoftware/wildstacker/objects/WStackedSnapshot.java
@@ -4,10 +4,12 @@ import com.bgsoftware.wildstacker.WildStackerPlugin;
 import com.bgsoftware.wildstacker.api.objects.StackedBarrel;
 import com.bgsoftware.wildstacker.api.objects.StackedSnapshot;
 import com.bgsoftware.wildstacker.api.objects.StackedSpawner;
+import com.bgsoftware.wildstacker.utils.Pair;
 import org.bukkit.Chunk;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.entity.EntityType;
+import org.bukkit.inventory.ItemStack;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -16,67 +18,22 @@ public final class WStackedSnapshot implements StackedSnapshot {
 
     private static final WildStackerPlugin plugin = WildStackerPlugin.getPlugin();
 
-    private final Map<Location, Map.Entry<Integer, EntityType>> stackedSpawners = new HashMap<>();
-    private final Map<Location, Map.Entry<Integer, Material>> stackedBarrels = new HashMap<>();
+    private final Map<Location, Pair<Integer, EntityType>> stackedSpawners = new HashMap<>();
+    private final Map<Location, Pair<Integer, ItemStack>> stackedBarrels = new HashMap<>();
 
     public WStackedSnapshot(Chunk chunk){
         for(StackedSpawner stackedSpawner : plugin.getSystemManager().getStackedSpawners(chunk)){
-            stackedSpawners.put(stackedSpawner.getLocation(), new Map.Entry<Integer, EntityType>() {
-                @Override
-                public Integer getKey() {
-                    return stackedSpawner.getStackAmount();
-                }
-
-                @Override
-                public EntityType getValue() {
-                    return stackedSpawner.getSpawnedType();
-                }
-
-                @Override
-                public EntityType setValue(EntityType value) {
-                    throw new UnsupportedOperationException("Cannot use setValue");
-                }
-            });
+            stackedSpawners.put(stackedSpawner.getLocation(), new Pair<>(stackedSpawner.getStackAmount(), stackedSpawner.getSpawnedType()));
         }
 
         for(StackedBarrel stackedBarrel : plugin.getSystemManager().getStackedBarrels(chunk)){
-            stackedBarrels.put(stackedBarrel.getLocation(), new Map.Entry<Integer, Material>() {
-                @Override
-                public Integer getKey() {
-                    return stackedBarrel.getStackAmount();
-                }
-
-                @Override
-                public Material getValue() {
-                    return stackedBarrel.getType();
-                }
-
-                @Override
-                public Material setValue(Material value) {
-                    throw new UnsupportedOperationException("Cannot use setValue");
-                }
-            });
+            stackedBarrels.put(stackedBarrel.getLocation(), new Pair<>(stackedBarrel.getStackAmount(), stackedBarrel.getBarrelItem(1)));
         }
     }
 
     @Override
     public Map.Entry<Integer, EntityType> getStackedSpawner(Location location) {
-        return stackedSpawners.getOrDefault(location, new Map.Entry<Integer, EntityType>() {
-            @Override
-            public Integer getKey() {
-                return 1;
-            }
-
-            @Override
-            public EntityType getValue() {
-                return null;
-            }
-
-            @Override
-            public EntityType setValue(EntityType value) {
-                return null;
-            }
-        });
+        return stackedSpawners.getOrDefault(location, new Pair<>(1, null));
     }
 
     @Override
@@ -86,22 +43,13 @@ public final class WStackedSnapshot implements StackedSnapshot {
 
     @Override
     public Map.Entry<Integer, Material> getStackedBarrel(Location location) {
-        return stackedBarrels.getOrDefault(location, new Map.Entry<Integer, Material>() {
-            @Override
-            public Integer getKey() {
-                return 1;
-            }
+        Map.Entry<Integer, ItemStack> pair = getStackedBarrelItem(location);
+        return new Pair<>(pair.getKey(), pair.getValue().getType());
+    }
 
-            @Override
-            public Material getValue() {
-                return Material.AIR;
-            }
-
-            @Override
-            public Material setValue(Material value) {
-                return null;
-            }
-        });
+    @Override
+    public Map.Entry<Integer, ItemStack> getStackedBarrelItem(Location location) {
+        return stackedBarrels.getOrDefault(location, new Pair<>(1, new ItemStack(Material.AIR)));
     }
 
     @Override
@@ -116,6 +64,13 @@ public final class WStackedSnapshot implements StackedSnapshot {
 
     @Override
     public Map<Location, Map.Entry<Integer, Material>> getAllBarrels() {
+        Map<Location, Map.Entry<Integer, Material>> map = new HashMap<>();
+        getAllBarrelsItems().forEach((location, pair) -> map.put(location, new Pair<>(pair.getKey(), pair.getValue().getType())));
+        return map;
+    }
+
+    @Override
+    public Map<Location, Map.Entry<Integer, ItemStack>> getAllBarrelsItems() {
         return new HashMap<>(stackedBarrels);
     }
 }
