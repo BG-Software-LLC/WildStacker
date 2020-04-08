bb78bb94712b7d752105cf94c68fbba6aba33ac9
OmerBenGera <omerthepro126@gmail.com>
2020-04-09 00:35:40 +0300

    Fixed break & place menus working when spawners stacking is disabled

--- src/main/java/com/bgsoftware/wildstacker/listeners/SpawnersListener.java
+++ src/main/java/com/bgsoftware/wildstacker/listeners/SpawnersListener.java
@@ -452,7 +452,8 @@ public final class SpawnersListener implements Listener {
 
     @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
     public void onSpawnerInteract(PlayerInteractEvent e){
-        if(e.getAction() != Action.RIGHT_CLICK_BLOCK || e.getClickedBlock().getType() != Materials.SPAWNER.toBukkitType())
+        if(e.getAction() != Action.RIGHT_CLICK_BLOCK || e.getClickedBlock().getType() != Materials.SPAWNER.toBukkitType() ||
+                !plugin.getSettings().spawnersStackingEnabled)
             return;
 
         StackedSpawner stackedSpawner = WStackedSpawner.of(e.getClickedBlock());
