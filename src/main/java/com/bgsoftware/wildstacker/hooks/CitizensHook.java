abef2d5a788008f4a95315456b8f3f18461c9bb6
OmerBenGera <omerthepro126@gmail.com>
2020-10-25 11:49:41 +0200

    Optimized NPC check for CitizensAPI

--- src/main/java/com/bgsoftware/wildstacker/hooks/CitizensHook.java
+++ src/main/java/com/bgsoftware/wildstacker/hooks/CitizensHook.java
@@ -1,19 +1,12 @@
 package com.bgsoftware.wildstacker.hooks;
 
 import net.citizensnpcs.api.CitizensAPI;
-import net.citizensnpcs.api.npc.NPCRegistry;
 import org.bukkit.entity.Entity;
 
 public final class CitizensHook {
 
     public static boolean isNPC(Entity entity){
-        if(PluginHooks.isCitizensEnabled) {
-            for (NPCRegistry registry : CitizensAPI.getNPCRegistries())
-                if (registry.isNPC(entity))
-                    return true;
-        }
-
-        return false;
+        return PluginHooks.isCitizensEnabled && CitizensAPI.getNPCRegistry().isNPC(entity);
     }
 
 }
