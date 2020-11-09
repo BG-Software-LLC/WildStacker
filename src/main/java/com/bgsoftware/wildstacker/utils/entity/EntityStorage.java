9a6a60505377c75018553d2239fd3b7041227c54
OmerBenGera <omerthepro126@gmail.com>
2020-11-09 19:29:52 +0200

    Fixed access to WeakHashMap from multiple threads

--- src/main/java/com/bgsoftware/wildstacker/utils/entity/EntityStorage.java
+++ src/main/java/com/bgsoftware/wildstacker/utils/entity/EntityStorage.java
@@ -6,42 +6,82 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 import java.util.WeakHashMap;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+import java.util.function.Consumer;
+import java.util.function.Function;
 
 public final class EntityStorage {
 
     private static final WeakHashMap<UUID, Map<String, Object>> entityStorage = new WeakHashMap<>();
+    private static final ReadWriteLock lock = new ReentrantReadWriteLock();
 
     public static void setMetadata(Entity entity, String key, Object value){
         UUID uuid = entity.getUniqueId();
-        entityStorage.computeIfAbsent(uuid, s -> new HashMap<>()).put(key, value);
+        write(entityStorage -> entityStorage.computeIfAbsent(uuid, s -> new HashMap<>()).put(key, value));
     }
 
     public static boolean hasMetadata(Entity entity, String key){
-        Map<String, Object> map = entityStorage.get(entity.getUniqueId());
+        Map<String, Object> map = read(entityStorage -> entityStorage.get(entity.getUniqueId()));
         return map != null && map.containsKey(key);
     }
 
     public static <T> T getMetadata(Entity entity, String key, Class<T> type){
         UUID uuid = entity.getUniqueId();
-        return hasMetadata(entity, key) ? type.cast(entityStorage.get(uuid).get(key)) : null;
+        return read(entityStorage -> {
+            Map<String, Object> map = entityStorage.get(uuid);
+            return map == null ? null : type.cast(map.get(key));
+        });
     }
 
     public static void removeMetadata(Entity entity, String key){
         UUID uuid = entity.getUniqueId();
-        if(entityStorage.containsKey(uuid))
-            entityStorage.get(uuid).remove(key);
+        write(entityStorage -> {
+            Map<String, Object> map = entityStorage.get(uuid);
+            if(map != null)
+                map.remove(key);
+        });
     }
 
     public static void clearMetadata(Entity entity){
-        Map<?, ?> map = entityStorage.remove(entity.getUniqueId());
+        Map<?, ?> map = writeAndGet(entityStorage -> entityStorage.remove(entity.getUniqueId()));
         if(map != null)
             map.clear();
     }
 
     public static void clearCache(){
-        for(Map map : entityStorage.values())
-            map.clear();
-        entityStorage.clear();
+        write(entityStorage -> {
+            for(Map map : entityStorage.values())
+                map.clear();
+            entityStorage.clear();
+        });
+    }
+
+    private static void write(Consumer<WeakHashMap<UUID, Map<String, Object>>> consumer){
+        try{
+            lock.writeLock().lock();
+            consumer.accept(entityStorage);
+        }finally {
+            lock.writeLock().unlock();
+        }
+    }
+
+    private static <R> R writeAndGet(Function<WeakHashMap<UUID, Map<String, Object>>, R> function){
+        try{
+            lock.writeLock().lock();
+            return function.apply(entityStorage);
+        }finally {
+            lock.writeLock().unlock();
+        }
+    }
+
+    private static <R> R read(Function<WeakHashMap<UUID, Map<String, Object>>, R> function){
+        try{
+            lock.readLock().lock();
+            return function.apply(entityStorage);
+        }finally {
+            lock.readLock().unlock();
+        }
     }
 
 }
