4098b840efc4c961a7ac3baf3e63327504661290
OmerBenGera <omerthepro126@gmail.com>
2020-12-09 13:09:35 +0200

    Fixed errors with EchoPet

--- src/main/java/com/bgsoftware/wildstacker/listeners/plugins/EchoPetListener.java
+++ src/main/java/com/bgsoftware/wildstacker/listeners/plugins/EchoPetListener.java
@@ -3,8 +3,9 @@ package com.bgsoftware.wildstacker.listeners.plugins;
 import com.bgsoftware.wildstacker.api.enums.SpawnCause;
 import com.bgsoftware.wildstacker.objects.WStackedEntity;
 import com.bgsoftware.wildstacker.utils.entity.EntityUtils;
+import com.bgsoftware.wildstacker.utils.threads.Executor;
 import com.dsh105.echopet.compat.api.event.PetPreSpawnEvent;
-import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Entity;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
@@ -13,9 +14,11 @@ public final class EchoPetListener implements Listener {
 
     @EventHandler
     public void onEchoPetSpawn(PetPreSpawnEvent e){
-        LivingEntity livingEntity = e.getPet().getEntityPet().getBukkitEntity();
-        if(EntityUtils.isStackable(livingEntity))
-            WStackedEntity.of(livingEntity).setSpawnCause(SpawnCause.ECHO_PET);
+        Executor.sync(() -> {
+            Entity entity = e.getPet().getEntityPet().getBukkitEntity();
+            if(EntityUtils.isStackable(entity))
+                WStackedEntity.of(entity).setSpawnCause(SpawnCause.ECHO_PET);
+        }, 1L);
     }
 
 }
