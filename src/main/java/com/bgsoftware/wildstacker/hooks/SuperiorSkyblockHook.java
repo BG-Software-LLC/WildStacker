1f4356426d29396e326d7df5d42dfad76a4e9794
OmerBenGera <omerthepro126@gmail.com>
2021-01-10 15:39:05 +0200

    Added support for SSB entity stacking setting to disallow stacked entities to spawn from spawners

--- src/main/java/com/bgsoftware/wildstacker/hooks/SuperiorSkyblockHook.java
+++ src/main/java/com/bgsoftware/wildstacker/hooks/SuperiorSkyblockHook.java
@@ -5,6 +5,7 @@ import com.bgsoftware.superiorskyblock.api.island.Island;
 import com.bgsoftware.superiorskyblock.api.island.IslandFlag;
 import com.bgsoftware.wildstacker.WildStackerPlugin;
 import com.bgsoftware.wildstacker.api.events.EntityStackEvent;
+import com.bgsoftware.wildstacker.api.events.SpawnerStackedEntitySpawnEvent;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
@@ -21,13 +22,20 @@ public final class SuperiorSkyblockHook {
         }
     }
 
-    private static class EntityListener implements Listener{
+    private static class EntityListener implements Listener {
 
         @EventHandler
         public void onEntityStack(EntityStackEvent e){
             Island island = SuperiorSkyblockAPI.getIslandAt(e.getTarget().getLocation());
             if(island != null && !island.hasSettingsEnabled(ENTITIES_STACKING))
                 e.setCancelled(true);
+        }
+
+        @EventHandler
+        public void onEntityStackedSpawn(SpawnerStackedEntitySpawnEvent e){
+            Island island = SuperiorSkyblockAPI.getIslandAt(e.getSpawner().getLocation());
+            if(island != null && !island.hasSettingsEnabled(ENTITIES_STACKING))
+                e.setShouldBeStacked(false);
         }
 
     }
