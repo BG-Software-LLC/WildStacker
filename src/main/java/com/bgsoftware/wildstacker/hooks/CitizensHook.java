73d0a3ab9a1bbbc87ab540c997938c8775092c47
OmerBenGera <omerthepro126@gmail.com>
2021-01-31 20:15:34 +0200

    Potential fix for citizens implementation not set

--- src/main/java/com/bgsoftware/wildstacker/hooks/CitizensHook.java
+++ src/main/java/com/bgsoftware/wildstacker/hooks/CitizensHook.java
@@ -1,12 +1,21 @@
 package com.bgsoftware.wildstacker.hooks;
 
+import net.citizensnpcs.Citizens;
 import net.citizensnpcs.api.CitizensAPI;
 import org.bukkit.entity.Entity;
+import org.bukkit.plugin.java.JavaPlugin;
 
 public final class CitizensHook {
 
+
     public static boolean isNPC(Entity entity){
-        return PluginHooks.isCitizensEnabled && CitizensAPI.getNPCRegistry().isNPC(entity);
+        if(!PluginHooks.isCitizensEnabled)
+            return false;
+
+        if(!CitizensAPI.hasImplementation())
+            CitizensAPI.setImplementation(JavaPlugin.getPlugin(Citizens.class));
+
+        return CitizensAPI.getNPCRegistry().isNPC(entity);
     }
 
 }
