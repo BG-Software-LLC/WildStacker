2fde577df6b2eae45371b1997834e83cc85a017a
OmerBenGera <omerthepro126@gmail.com>
2021-04-10 09:01:21 +0300

    Fixed null errors with checking for slimes sizes

--- src/main/java/com/bgsoftware/wildstacker/listeners/EntitiesListener.java
+++ src/main/java/com/bgsoftware/wildstacker/listeners/EntitiesListener.java
@@ -516,8 +516,8 @@ public final class EntitiesListener implements Listener {
             EntitiesGetter.getNearbyEntities(e.getEntity().getLocation(), 2,
                     entity -> entity instanceof Slime && originalSize * 2 == ((Slime) entity).getSize() && EntityStorage.hasMetadata(entity, EntityFlag.ORIGINAL_AMOUNT))
                     .stream().findFirst().ifPresent(entity -> {
-                StackedEntity stackedEntity = WStackedEntity.of(e.getEntity());
-                stackedEntity.setStackAmount(stackedEntity.getFlag(EntityFlag.ORIGINAL_AMOUNT), true);
+                int originalAmount = EntityStorage.getMetadata(entity, EntityFlag.ORIGINAL_AMOUNT);
+                WStackedEntity.of(e.getEntity()).setStackAmount(originalAmount, true);
             });
         }
     }
