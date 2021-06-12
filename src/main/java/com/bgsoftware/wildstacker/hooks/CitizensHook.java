547807c88cdd018366e79de29662370f8e5870ae
OmerBenGera <omerthepro126@gmail.com>
2021-06-12 13:46:18 +0300

    Fixed potential code breaking with Citizens

--- src/main/java/com/bgsoftware/wildstacker/hooks/CitizensHook.java
+++ src/main/java/com/bgsoftware/wildstacker/hooks/CitizensHook.java
@@ -1,19 +1,14 @@
 package com.bgsoftware.wildstacker.hooks;
 
-import net.citizensnpcs.Citizens;
 import net.citizensnpcs.api.CitizensAPI;
 import org.bukkit.entity.Entity;
-import org.bukkit.plugin.java.JavaPlugin;
 
 public final class CitizensHook {
 
 
     public static boolean isNPC(Entity entity){
-        if(!PluginHooks.isCitizensEnabled)
+        if(!PluginHooks.isCitizensEnabled || !CitizensAPI.hasImplementation())
             return false;
-
-        if(!CitizensAPI.hasImplementation())
-            CitizensAPI.setImplementation(JavaPlugin.getPlugin(Citizens.class));
 
         return CitizensAPI.getNPCRegistry().isNPC(entity);
     }
