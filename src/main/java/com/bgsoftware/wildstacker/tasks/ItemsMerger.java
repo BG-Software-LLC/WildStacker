0fa5a46ab556756923590d82863a1fa9584c6f3b
OmerBenGera <omerthepro126@gmail.com>
2021-07-08 18:29:49 +0300

    Fix items merger task is called async causing errors

--- src/main/java/com/bgsoftware/wildstacker/tasks/ItemsMerger.java
+++ src/main/java/com/bgsoftware/wildstacker/tasks/ItemsMerger.java
@@ -10,9 +10,6 @@ import org.bukkit.entity.Item;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.scheduler.BukkitTask;
 
-import java.util.Set;
-import java.util.concurrent.ConcurrentHashMap;
-
 public final class ItemsMerger extends BukkitRunnable {
 
     private static final WildStackerPlugin plugin = WildStackerPlugin.getPlugin();
@@ -21,7 +18,7 @@ public final class ItemsMerger extends BukkitRunnable {
 
     private ItemsMerger(){
         if(plugin.getSettings().itemsStackingEnabled && plugin.getSettings().itemsStackInterval > 0)
-            task = runTaskTimerAsynchronously(plugin, plugin.getSettings().itemsStackInterval, plugin.getSettings().itemsStackInterval);
+            task = runTaskTimer(plugin, plugin.getSettings().itemsStackInterval, plugin.getSettings().itemsStackInterval);
     }
 
     public static void start(){
@@ -36,10 +33,7 @@ public final class ItemsMerger extends BukkitRunnable {
         if(Bukkit.getOnlinePlayers().size() > 0) {
             for(World world : Bukkit.getWorlds()){
                 try {
-                    Set<Item> items = ConcurrentHashMap.newKeySet();
-                    items.addAll(world.getEntitiesByClass(Item.class));
-
-                    for (Item item : items) {
+                    for (Item item : world.getEntitiesByClass(Item.class)) {
                         try {
                             if (!ItemUtils.isStackable(item))
                                 continue;
