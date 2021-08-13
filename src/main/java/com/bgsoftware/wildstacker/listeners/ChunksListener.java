47080b64d1db0fcc606e282d1a1f9dec1fe22af0
OmerBenGera <omerthepro126@gmail.com>
2021-08-13 19:26:29 +0300

    Fixed detection of unloaded worlds causing unnecessary ram usage

--- src/main/java/com/bgsoftware/wildstacker/listeners/ChunksListener.java
+++ src/main/java/com/bgsoftware/wildstacker/listeners/ChunksListener.java
@@ -1,11 +1,14 @@
 package com.bgsoftware.wildstacker.listeners;
 
 import com.bgsoftware.wildstacker.WildStackerPlugin;
+import org.bukkit.Chunk;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.world.ChunkLoadEvent;
 import org.bukkit.event.world.ChunkUnloadEvent;
+import org.bukkit.event.world.WorldLoadEvent;
+import org.bukkit.event.world.WorldUnloadEvent;
 
 @SuppressWarnings("unused")
 public final class ChunksListener implements Listener {
@@ -23,10 +26,28 @@ public final class ChunksListener implements Listener {
             plugin.getSystemManager().handleChunkUnload(e.getChunk());
     }
 
-    @EventHandler
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
     public void onChunkLoad(ChunkLoadEvent e) {
         if (loadedData)
             plugin.getSystemManager().handleChunkLoad(e.getChunk());
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+    public void onWorldUnload(WorldUnloadEvent e){
+        if (loadedData){
+            for(Chunk chunk : e.getWorld().getLoadedChunks()){
+                plugin.getSystemManager().handleChunkUnload(chunk);
+            }
+        }
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+    public void onWorldLoad(WorldLoadEvent e){
+        if (loadedData){
+            for(Chunk chunk : e.getWorld().getLoadedChunks()){
+                plugin.getSystemManager().handleChunkLoad(chunk);
+            }
+        }
     }
 
 }
