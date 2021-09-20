7be724def35f5546294d66adb14842c5d3c137ca
OmerBenGera <omerthepro126@gmail.com>
2021-09-20 13:59:16 +0300

    Temporary removed clearing of Metadata due to performance issues

--- src/main/java/com/bgsoftware/wildstacker/utils/entity/EntityUtils.java
+++ src/main/java/com/bgsoftware/wildstacker/utils/entity/EntityUtils.java
@@ -12,8 +12,6 @@ import com.bgsoftware.wildstacker.hooks.MythicMobsHook;
 import com.bgsoftware.wildstacker.hooks.PluginHooks;
 import com.bgsoftware.wildstacker.utils.ServerVersion;
 import com.bgsoftware.wildstacker.utils.legacy.EntityTypes;
-import com.bgsoftware.wildstacker.utils.threads.Executor;
-import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.Material;
@@ -581,15 +579,15 @@ public final class EntityUtils {
     }
 
     public static void clearBukkitMetadata(UUID entityUUID) {
-        if(!Bukkit.isPrimaryThread()) {
-            Executor.sync(() -> clearBukkitMetadata(entityUUID));
-            return;
-        }
-
-        MetadataStoreBase<Entity> metadataStoreBase = plugin.getNMSAdapter().getEntityMetadataStore();
-        Map<String, ?> metadataMap = METADATA_STORAGE.get(metadataStoreBase);
-        String entityUUIDString = entityUUID.toString();
-        metadataMap.entrySet().removeIf(entry -> entry.getKey().contains(entityUUIDString));
+//        if(!Bukkit.isPrimaryThread()) {
+//            Executor.sync(() -> clearBukkitMetadata(entityUUID));
+//            return;
+//        }
+//
+//        MetadataStoreBase<Entity> metadataStoreBase = plugin.getNMSAdapter().getEntityMetadataStore();
+//        Map<String, ?> metadataMap = METADATA_STORAGE.get(metadataStoreBase);
+//        String entityUUIDString = entityUUID.toString();
+//        metadataMap.keySet().removeIf(key -> key.contains(entityUUIDString));
     }
 
     private static void addDropArmor(List<ItemStack> drops, LivingEntity livingEntity, ItemStack itemStack, int lootBonusLevel, double dropChance) {
