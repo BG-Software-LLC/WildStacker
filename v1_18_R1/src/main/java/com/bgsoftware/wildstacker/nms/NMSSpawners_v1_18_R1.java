a956e148475540e44ba925392bb83b7b7ebfeff2
OmerBenGera <omerthepro126@gmail.com>
2021-11-27 20:19:07 +0200

    Implement nms-methods for 1.18

---
+++ v1_18_R1/src/main/java/com/bgsoftware/wildstacker/nms/NMSSpawners_v1_18_R1.java
@@ -0,0 +1,18 @@
+package com.bgsoftware.wildstacker.nms;
+
+import com.bgsoftware.wildstacker.api.objects.StackedSpawner;
+
+@SuppressWarnings("unused")
+public final class NMSSpawners_v1_18_R1 implements NMSSpawners {
+
+    @Override
+    public boolean updateStackedSpawner(StackedSpawner stackedSpawner) {
+        throw new UnsupportedOperationException("Not supported in 1.17.");
+    }
+
+    @Override
+    public void registerSpawnConditions() {
+        throw new UnsupportedOperationException("Not supported in 1.17.");
+    }
+
+}
