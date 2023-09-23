b1c5bcfaf292801fd6bed281030d6ecf9c8260ec
OmerBenGera <omerthepro126@gmail.com>
2023-09-23 13:51:10 +0300

    Added support to 1.20.2

--- src/main/java/com/bgsoftware/wildstacker/WildStackerPlugin.java
+++ src/main/java/com/bgsoftware/wildstacker/WildStackerPlugin.java
@@ -221,7 +221,8 @@ public final class WildStackerPlugin extends JavaPlugin implements WildStacker {
                     new Pair<>(2975, "v1_18"),
                     new Pair<>(3336, null),
                     new Pair<>(3337, "v1_19"),
-                    new Pair<>(3465, "v1_20_1")
+                    new Pair<>(3465, "v1_20_1"),
+                    new Pair<>(3578, "v1_20_2")
             );
 
             for (Pair<Integer, String> versionData : versions) {
