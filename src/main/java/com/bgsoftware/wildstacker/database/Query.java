89dac5a0f767e71175044c05c0355f81d1148448
nivcoo <nico64800@live.fr>
2025-09-12 10:26:49 +0200

    improv. now it works with db prefix

--- src/main/java/com/bgsoftware/wildstacker/database/Query.java
+++ src/main/java/com/bgsoftware/wildstacker/database/Query.java
@@ -2,11 +2,11 @@ package com.bgsoftware.wildstacker.database;
 
 public enum Query {
 
-    SPAWNER_INSERT("REPLACE INTO spawners VALUES(?, ?, ?);", 3),
-    SPAWNER_DELETE("DELETE FROM spawners WHERE location=?;", 1),
+    SPAWNER_INSERT("REPLACE INTO {prefix}spawners VALUES(?, ?, ?);", 3),
+    SPAWNER_DELETE("DELETE FROM {prefix}spawners WHERE location=?;", 1),
 
-    BARREL_INSERT("REPLACE INTO barrels VALUES(?, ?, ?);", 3),
-    BARREL_DELETE("DELETE FROM barrels WHERE location=?;", 1);
+    BARREL_INSERT("REPLACE INTO {prefix}barrels VALUES(?, ?, ?);", 3),
+    BARREL_DELETE("DELETE FROM {prefix}barrels WHERE location=?;", 1);
 
     private final String query;
     private final int parametersCount;
