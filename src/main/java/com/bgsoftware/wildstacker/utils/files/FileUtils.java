80d712a7e468469715a1f404814e53c53f6f913b
Lemvs <48791834+Lemvs@users.noreply.github.com>
2025-12-31 13:50:02 +0100

    Fix import

--- src/main/java/com/bgsoftware/wildstacker/utils/files/FileUtils.java
+++ src/main/java/com/bgsoftware/wildstacker/utils/files/FileUtils.java
@@ -15,7 +15,10 @@ import javax.annotation.Nullable;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public final class FileUtils {
 
