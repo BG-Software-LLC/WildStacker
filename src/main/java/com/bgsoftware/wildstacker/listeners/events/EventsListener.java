4bd386009a892600f0c08056b3e258b8d87bc99c
Lemvs <aleksy.gajewski@wp.pl>
2026-08-25 20:40:05 +0200

    Fixed and improved "multiply-..." options

--- src/main/java/com/bgsoftware/wildstacker/listeners/events/EventsListener.java
+++ src/main/java/com/bgsoftware/wildstacker/listeners/events/EventsListener.java
@@ -3,9 +3,11 @@ package com.bgsoftware.wildstacker.listeners.events;
 import com.bgsoftware.wildstacker.WildStackerPlugin;
 import com.bgsoftware.wildstacker.utils.ServerVersion;
 import com.bgsoftware.wildstacker.utils.entity.EntitiesGetter;
+import com.bgsoftware.wildstacker.utils.entity.EntityUtils;
 import com.bgsoftware.wildstacker.utils.legacy.Materials;
 import org.bukkit.Material;
 import org.bukkit.entity.Chicken;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Item;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -13,71 +15,99 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.ItemSpawnEvent;
 
 import javax.annotation.Nullable;
-import java.util.function.Supplier;
 
 public final class EventsListener {
 
     private static WildStackerPlugin plugin;
 
     @Nullable
-    private static IEggLayListener eggLayListener;
+    private static IArmadilloScuteDropListener armadilloScuteDropListener;
     @Nullable
-    private static IScuteDropListener scuteDropListener;
+    private static IChickenEggLayListener chickenEggLayListener;
+    @Nullable
+    private static ITurtleScuteDropListener turtleScuteDropListener;
 
     public static void register(WildStackerPlugin plugin) {
         EventsListener.plugin = plugin;
 
-        plugin.getServer().getPluginManager().registerEvents(new EggLay(), plugin);
+        if (ServerVersion.isAtLeast(ServerVersion.v1_20)) {
+            plugin.getServer().getPluginManager().registerEvents(new ArmadilloScuteDrop(), plugin);
+        }
 
-        if (ServerVersion.isAtLeast(ServerVersion.v1_13))
-            plugin.getServer().getPluginManager().registerEvents(new ScuteDrop(), plugin);
+        plugin.getServer().getPluginManager().registerEvents(new ChickenEggLay(), plugin);
+
+        if (ServerVersion.isAtLeast(ServerVersion.v1_13)) {
+            plugin.getServer().getPluginManager().registerEvents(new TurtleScuteDrop(), plugin);
+        }
     }
 
-    public static void registerEggLayListener(IEggLayListener eggLayListener) {
-        EventsListener.eggLayListener = eggLayListener;
+    public static void registerArmadilloScuteDropListener(IArmadilloScuteDropListener armadilloScuteDropListener) {
+        EventsListener.armadilloScuteDropListener = armadilloScuteDropListener;
     }
 
-    public static void registerScuteDropListener(IScuteDropListener scuteDropListener) {
-        EventsListener.scuteDropListener = scuteDropListener;
+    public static void registerChickenEggLayListener(IChickenEggLayListener eggLayListener) {
+        EventsListener.chickenEggLayListener = eggLayListener;
     }
 
-    private static class EggLay implements Listener {
+    public static void registerTurtleScuteDropListener(ITurtleScuteDropListener turtleScuteDropListener) {
+        EventsListener.turtleScuteDropListener = turtleScuteDropListener;
+    }
+
+    private static class ArmadilloScuteDrop implements Listener {
+
+        @Nullable
+        private static final EntityType ARMADILLO = EntityUtils.getEntityTypeSafe("ARMADILLO");
+        @Nullable
+        private static final Material ARMADILLO_SCUTE = Materials.getMaterialOrNull("ARMADILLO_SCUTE");
 
         @EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
-        public void onEggLay(ItemSpawnEvent e) {
-            if (eggLayListener == null || !Materials.isChickenEgg(e.getEntity().getItemStack()))
+        public void onArmadilloScuteDrop(ItemSpawnEvent e) {
+            if (armadilloScuteDropListener == null || e.getEntity().getItemStack().getType() != ARMADILLO_SCUTE) {
                 return;
+            }
 
-            Item egg = e.getEntity();
+            Item armadilloScute = e.getEntity();
 
-            EntitiesGetter.getNearbyEntities(e.getEntity().getLocation(), 2, entity ->
-                    entity instanceof Chicken && plugin.getNMSEntities().getEggLayTime((Chicken) entity) <= 0
-            ).findFirst().ifPresent(chicken -> eggLayListener.apply((Chicken) chicken, egg));
+            EntitiesGetter.getNearbyEntities(e.getEntity().getLocation(), 2,
+                            entity -> entity.getType() == ARMADILLO)
+                    .findFirst().ifPresent(armadillo -> armadilloScuteDropListener.apply(armadillo, armadilloScute));
         }
 
     }
 
-    private static class ScuteDrop implements Listener {
-
-        @Nullable
-        private static final Material SCUTE = ((Supplier<Material>) () -> {
-            try {
-                return Material.valueOf("SCUTE");
-            } catch (IllegalArgumentException error) {
-                return null;
-            }
-        }).get();
+    private static class ChickenEggLay implements Listener {
 
         @EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
-        public void onScuteDrop(ItemSpawnEvent e) {
-            if (scuteDropListener == null || e.getEntity().getItemStack().getType() != SCUTE)
+        public void onChickenEggLay(ItemSpawnEvent e) {
+            if (chickenEggLayListener == null || !Materials.isChickenEgg(e.getEntity().getItemStack())) {
                 return;
+            }
 
-            Item scute = e.getEntity();
+            Item egg = e.getEntity();
+
+            EntitiesGetter.getNearbyEntities(e.getEntity().getLocation(), 2, entity ->
+                            entity instanceof Chicken && plugin.getNMSEntities().getEggLayTime((Chicken) entity) <= 0)
+                    .findFirst().ifPresent(chicken -> chickenEggLayListener.apply((Chicken) chicken, egg));
+        }
+
+    }
+
+    private static class TurtleScuteDrop implements Listener {
+
+        @Nullable
+        private static final Material TURTLE_SCUTE = Materials.getMaterialOrNull("TURTLE_SCUTE", "SCUTE");
+
+        @EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
+        public void onTurtleScuteDrop(ItemSpawnEvent e) {
+            if (turtleScuteDropListener == null || e.getEntity().getItemStack().getType() != TURTLE_SCUTE) {
+                return;
+            }
+
+            Item turtleScute = e.getEntity();
 
             EntitiesGetter.getNearbyEntities(e.getEntity().getLocation(), 2,
                             entity -> entity instanceof org.bukkit.entity.Turtle)
-                    .findFirst().ifPresent(turtle -> scuteDropListener.apply(turtle, scute));
+                    .findFirst().ifPresent(turtle -> turtleScuteDropListener.apply(turtle, turtleScute));
         }
 
     }
--- src/main/java/com/bgsoftware/wildstacker/listeners/events/IArmadilloScuteDropListener.java
+++ src/main/java/com/bgsoftware/wildstacker/listeners/events/IArmadilloScuteDropListener.java
@@ -3,8 +3,8 @@ package com.bgsoftware.wildstacker.listeners.events;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Item;
 
-public interface IScuteDropListener {
+public interface IArmadilloScuteDropListener {
 
-    void apply(Entity turtle, Item scute);
+    void apply(Entity armadillo, Item armadilloScute);
 
 }
